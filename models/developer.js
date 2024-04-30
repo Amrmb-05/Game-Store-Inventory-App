5644af2a6901c11963f8eee9e945fdc478058881
amrmohamed <amoree.mb@gmail.com>
2024-04-30 15:56:49 +0300

    Define developer schema

---
+++ models/developer.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DevSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+DevSchema.virtual("url").get(function () {
+  return `/catalog/dev/${this._id}`;
+});
