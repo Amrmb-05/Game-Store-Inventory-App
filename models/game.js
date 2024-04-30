45ab245c983cdd159b85d8cc9d810e0a4511d233
amrmohamed <amoree.mb@gmail.com>
2024-04-30 16:42:50 +0300

    Fix typo in the naming of GameSchema variable

--- models/game.js
+++ models/game.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const GameScheema = new Schema({
+const GameSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
