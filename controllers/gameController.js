5ed89ad109eda4656800d62074a8aab9c423dbaa
amrmohamed <amoree.mb@gmail.com>
2024-05-02 23:12:12 +0300

    Write controller function to post game form data

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -88,3 +88,90 @@ exports.game_create_get = asyncHandler(async (req, res, next) => {
     platforms: platforms,
   });
 });
+
+exports.game_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+    }
+    next();
+    // if (!Array.isArray(req.body.platform)) {
+    //   req.body.platform =
+    //     typeof req.body.platform === "undefined" ? [] : [req.body.platform];
+    // }
+    // next();
+  },
+
+  body("title", "Title must not be empty").trim().isLength({ min: 3 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  body("developer", "Developer must not be empty")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("price", "Price must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("inStock", "inStock must bot be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("genre.*").escape(),
+  body("platform.*").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    // Get validation errors from the request
+    const errors = validationResult(req);
+
+    const game = new Game({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      inStock: req.body.inStock,
+      developer: req.body.developer,
+      genre: req.body.genre,
+      platform: req.body.platform,
+    });
+
+    if (!errors.isEmpty()) {
+      const [devs, genres] = await Promise.all([
+        Dev.find({}).sort({ name: 1 }).exec(),
+        Genre.find({}).sort({ name: 1 }).exec(),
+      ]);
+      const platforms = [
+        "PS5",
+        "PS4",
+        "Xbox Series X",
+        "Xbox One",
+        "Windows",
+        "Nintendo Switch",
+      ];
+
+      // Mark our selected genres as checked.
+      for (const genre of genres) {
+        if (game.genre.includes(genre._id)) {
+          genre.checked = "true";
+        }
+      }
+
+      for (const platform of platforms) {
+        if (game.platform.includes(platform)) {
+          platform.checked = "true";
+        }
+      }
+      res.render("game_form", {
+        title: "Add Game",
+        devs: devs,
+        genres: genres,
+        platforms: platforms,
+        game: game,
+        errors: errors.array(),
+      });
+    } else {
+      await game.save();
+      res.redirect(game.url);
+    }
+  }),
+];
