f8043119a7549e29c13dcc67850f42a20292659d
amrmohamed <amoree.mb@gmail.com>
2024-05-02 21:21:30 +0300

    Write dev_details controller function

--- controllers/devController.js
+++ controllers/devController.js
@@ -8,3 +8,18 @@ exports.dev_list = asyncHandler(async (req, res, next) => {
 
   res.render("dev_list", { title: "Developers", devs: devs });
 });
+
+exports.dev_detail = asyncHandler(async (req, res, next) => {
+  const [dev, dev_games] = await Promise.all([
+    Dev.findById(req.params.id).exec(),
+    Game.find({ developer: req.params.id }).sort({ title: 1 }).exec(),
+  ]);
+
+  if (dev === null) {
+    const err = new Error("Developer not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("dev_detail", { dev: dev, dev_games: dev_games });
+});
