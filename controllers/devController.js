00d751a794a48ed9abb9d6f957036b09f335947e
amrmohamed <amoree.mb@gmail.com>
2024-05-03 17:16:57 +0300

    Write controller functions to handle updating the developer

--- controllers/devController.js
+++ controllers/devController.js
@@ -61,3 +61,48 @@ exports.dev_create_post = [
     }
   }),
 ];
+
+exports.dev_update_get = asyncHandler(async (req, res, next) => {
+  const dev = await Dev.findById(req.params.id).exec();
+  if (dev === null) {
+    const err = new Error("Developer was npt found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("dev_form", { title: "Update Developer", dev: dev });
+});
+
+exports.dev_update_post = [
+  body("name", "Name must be atleast 3 characters long")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const dev = new Dev({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("dev_form", {
+        title: "Add Developer",
+        dev: dev,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const devExists = await Dev.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (devExists) {
+        res.redirect(devExists.url);
+      } else {
+        await Dev.findByIdAndUpdate(req.params.id, dev);
+        res.redirect(dev.url);
+      }
+    }
+  }),
+];
