7126fd582b1286be243af1698bc770621bc8310c
amrmohamed <amoree.mb@gmail.com>
2024-05-03 15:08:25 +0300

    Write controller functions to get the add genre form and post its data

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,7 @@
 const Genre = require("../models/genre");
 const Game = require("../models/game");
 const asyncHandler = require("express-async-handler");
-const { body, validatonResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
   const genres = await Genre.find({}).sort({ name: 1 }).exec();
@@ -19,3 +19,41 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 
   res.render("genre_detail", { genre: genre, genre_games: genre_games });
 });
+
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", { title: "Add Genre" });
+};
+
+exports.genre_create_post = [
+  body("name", "name must be more than 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty()) {
+      req.render("genre_form", {
+        title: "Add Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+    } else {
+      // Check if genre exists
+      const genreExists = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (genreExists) {
+        res.redirect(genreExists.url);
+      } else {
+        await genre.save();
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
